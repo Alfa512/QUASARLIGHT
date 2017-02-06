e4da3c04cd6959414b8f7f934c73f2521f09fa55
ALFA <ALFA@ALFA-G73>
2017-02-06 16:26:58 +0300

    About Us Page Changes

---
+++ QuasarLight.Data.Model/DataModel/Citation.cs
@@ -0,0 +1,10 @@
+﻿namespace QuasarLight.Data.Model.DataModel
+{
+    public class Citation
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public string Сreation { get; set; }
+    }
+}
